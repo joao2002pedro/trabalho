4a4bfafdafbec9c938f0d0386532616dd9a9e780
JoaoR <joaopedrocorrea160@gmail.com>
2022-11-28 20:05:58 -0300

    Codigo

---
+++ src/test/java/Trabalho/Empresarial/EmpresarialApplicationTests.java
@@ -0,0 +1,13 @@
+package Trabalho.Empresarial;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpresarialApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
